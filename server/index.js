94ac6d1d780320c827677b2a4a88ab72c3c8ecfa
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2023-08-08 09:05:32 +0600

    Add server proof and client request

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
+const fs = require('fs')
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
 
-const port = 1225;
+const port = 8225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const names = JSON.parse(fs.readFileSync('../utils/niceList.json').toString());
+const tree = new MerkleTree(names);
+
+
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const { name } = req.body;
+  const nameIndex = names.indexOf(name);
+  
+  if (nameIndex < 0) {
+    return res.send("You are not on the list :(");
+  }
+  
+  const proof = tree.getProof(nameIndex);
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
